49b6aac9be65f9611fcb2cb175d97ff8af26e80c
Archie Maclean <archiepiper@hotmail.com>
2019-09-28 09:53:44 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import speech_rec, os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv('.env')
+
+prefix = "#"
+bot = commands.Bot(command_prefix=prefix)
+
+
+@bot.event
+async def on_ready():
+    print("Everything's all ready to go~")
+
+
+@bot.event
+async def on_message(message):
+	print("The message's content was", message.content)
+	await bot.process_commands(message)
+
+
+@bot.command()
+async def start(ctx):
+    '''
+    This text will be shown in the help command
+    '''
+    await ctx.send('Started')
+
+bot.run(os.getenv('TOKEN'))
