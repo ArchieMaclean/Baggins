72a07cd93dd5898b3595e06ad9f7894c78b2b2e8
Archie Maclean <archiepiper@hotmail.com>
2019-09-29 09:10:20 +0100

    FINALLY got audio record working

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 const Discord = require('discord.js');
+const { Readable } = require('stream');
 require('dotenv').config();
+const fs = require('fs');
+const opus = require('node-opus');
+
+
+const SILENCE_FRAME = Buffer.from([0xF8,0xFF,0xFE]);
+
+class Silence extends Readable {
+    _read() {
+        this.push(SILENCE_FRAME);
+    }
+}
 
 const client = new Discord.Client();
 const prefix = '&';
 
 const connected_channels = [];
+const log_channel = 'log'
 
+function logAllEmitterEvents(eventEmitter)
+{
+    var emitToLog = eventEmitter.emit;
+
+    eventEmitter.emit = function () {
+        var event = arguments[0];
+        console.log("event emitted: " + event);
+        emitToLog.apply(eventEmitter, arguments);
+    }
+}
+
+
+const log = message => {
+    const ch = client.channels.find('name',log_channel);
+    if (ch) ch.send(message);
+}
+
+const help = msg => {
+    msg.channel.send('Recorder');
+}
 
 const start = message => {
     if (!message.member.voiceChannel) {
@@ -15,16 +48,40 @@ const start = message => {
     }
     message.member.voiceChannel.join()
     .then(c => {
-        connected_channels.push(c)
+        c.playOpusStream(new Silence());
+        const receiver = c.createReceiver();
+        c.on('speaking', (user, speaking) => {
+            if (speaking) {
+                let stream;
+                let outStream;
+                stream = receiver.createPCMStream(user);
+                console.log(stream);
+                // logAllEmitterEvents(stream);
+                outStream = fs.createWriteStream('./data.pcm');
+                stream.pipe(outStream);
+                stream.on('readable', data => {
+                    console.log(`readable: ${stream.read()}`);
+                });
+                stream.on('resume', data => {
+                    console.log(`resume: ${stream.read()}`);
+                });
+                stream.on('end',_ => {
+                    console.log('ended');
+                    outStream.end();
+                });
+            }
+        });
+        connected_channels.push(c);
         message.channel.send('Connected!');
     })
     .catch(e => message.channel.send(`The following error occurred: ${e}`));
 }
 
-const stop = _ => {
+const stop = message => {
     connected_channels.forEach(c => {
         c.disconnect();
     });
+    if (message) message.channel.send('Disconnected.')
 }
 
 const run = (message) => {
@@ -32,14 +89,15 @@ const run = (message) => {
         case 'start':
             start(message); break;
         case 'stop':
-            stop(); break;
+            stop(message); break;
         default:
-            help(); break;
+            help(message); break;
     }
 }
 
 client.on('ready', () => {
     console.log('Ready!');
+    stop();
 });
 
 client.on('message', message => {
--- speech_rec.py
+++ speech_rec.py
@@ -1,15 +1,22 @@
 # OLD (probably)
 
 import speech_recognition as sr
-class SpeechRec:
-	def __init__(self):
-		r = sr.Recognizer()
-		with sr.AudioFile('untitled.wav') as source:
-			audio = r.record(source)
+import wave
 
-		try:
-			print("You said " + r.recognize_google(audio))
-		except sr.UnknownValueError:
-			print("Could not understand audio")
-		except sr.RequestError as e:
-			print("Could not request results; {0}".format(e))
+# Speech Recognition wants it as a wav file, so convert
+with open('data.pcm', 'rb') as pcmfile:
+	pcmdata = pcmfile.read()
+with wave.open('data.wav', 'wb') as wavfile:
+	wavfile.setparams((2, 2, 44100, 0, 'NONE', 'NONE'))
+	wavfile.writeframes(pcmdata)
+
+r = sr.Recognizer()
+with sr.AudioFile('data.wav') as source:
+	audio = r.record(source)
+
+try:
+	print("You said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+	print("Could not understand audio")
+except sr.RequestError as e:
+	print("Could not request results; {0}".format(e))
